735772372a251c9dbdab722f2a33e1adda78255c
joaluzmila <86448563+joaluzmila@users.noreply.github.com>
2021-09-28 20:14:37 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var ball;
+var hypnoticBall, database, position;
+
+function setup(){ 
+    database = firebase.database();
+    console.log(database);
+
+    createCanvas(300,300);
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+    var hypnoticBallPosition = database.ref('ball/position');
+    hypnoticBallPosition.on("value",readPosition,showError); 
+}
+
+function draw(){
+    background("white");
+    if(position !== undefined){
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+}
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+
+function readPosition(){
+    position =  data.val();
+    console.log(position.x);
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+}
+
+function writePosition(){
+    database.read('ball/position').set({
+        'x' : position.x + x,
+        'y' : position.y + y
+    })
+}
+
+function showError(){
+    console.log("error")
+}
